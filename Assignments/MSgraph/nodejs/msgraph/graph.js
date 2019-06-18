ced5e5b84fb574bf0f279b9b8152d3e3ea3ea351
Thangeswari <spthangu2016@gmail.com>
2019-06-19 03:57:52 +0530

    s

--- Assignments/MSgraph/nodejs/msgraph/graph.js
+++ Assignments/MSgraph/nodejs/msgraph/graph.js
@@ -12,7 +12,7 @@ module.exports = {
     const client = getAuthenticatedClient(accessToken);
   
     const listitems = await client
-      .api('/sites/thangeswari.sharepoint.com,519834a7-fdb4-4739-9da5-a98a4cf0a378,3c9b43e0-b765-45bb-83d7-f32463fcc0fd/lists/7d1ca4cb-f44b-4c3e-8a21-7ab56e8ec2c2/items')
+      .api('/sites/xxx.sharepoint.com,xxx,xxx/lists/xxx/items')
       .expand('fields')
       .select('id')     
       .get();
