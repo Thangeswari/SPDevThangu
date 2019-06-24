dc9127becb736c3474ef8411879d99fc2b75082c
Thangeswari <spthangu2016@gmail.com>
2019-06-25 05:10:45 +0530

    Graph

---
+++ Assignments/MSgraph/MsGraphQueriesO365/Queries.ps1
@@ -0,0 +1,53 @@
+﻿https://graph.microsoft.com/v1.0/users
+https://graph.microsoft.com/v1.0/me/photo
+https://graph.microsoft.com/v1.0/me/photo
+UserActivity.ReadWrite.CreatedByApp
+https://graph.microsoft.com/v1.0/me/activities
+
+https://graph.microsoft.com/v1.0/me/calendar/events
+
+https://graph.microsoft.com/beta/me/messages
+{
+    "subject":"Draft Message?",
+    "importance":"High",
+    "body":{
+        "contentType":"HTML",
+        "content":"They were <b>awesome</b>!"
+    },
+    "toRecipients":[
+        {
+            "emailAddress":{
+                "address":"admin@xxx.onmicrosoft.com"
+            }
+        }
+    ]
+}
+https://graph.microsoft.com/v1.0/me/sendMail
+{
+  "message": {
+    "subject": "You have a Payout",
+    "body": {
+      "contentType": "HTML",
+      "content": "You have received <b>1000$</b> from XYZ for your Product or services."
+    },
+    "toRecipients": [
+      {
+        "emailAddress": {
+          "address": "admin@xxx.onmicrosoft.com"
+        }
+      }
+    ]
+  }
+}
+
+https://graph.microsoft.com/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq 'Team')
+
+
+https://graph.microsoft.com/v1.0/me/insights/trending?$filter=ResourceVisualization/Type eq 'PowerPoint'
+
+https://graph.microsoft.com/beta/me/insights/used
+
+https://graph.microsoft.com/v1.0/me/contacts
+
+
+
---
+++ Assignments/MSgraph/PowerShell/PowerShellGraphDemo.ps1
@@ -0,0 +1,94 @@
+﻿Connect-PnPMicrosoftGraph -Scopes "Sites.FullControl.All"
+#Give Credentials to your SharePoint site and Accept
+$accessToken = Get-PnPAccessToken
+
+$apiUrl = "https://graph.microsoft.com/beta/sites/xxx.sharepoint.com:/sites/Assignments/PsThangu"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Get
+$results
+$siteId=$results.id
+
+#Lists Create List names Fruits
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists"
+$data=@"
+{
+  "displayName": "FruitsPS",
+  
+  "columns": [
+    {
+      "name": "Location",
+      "text": { }
+    }
+  ],
+  "list":{
+    "template": "genericList"
+  }
+}
+"@
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Post -Body $data -ContentType "application/json"
+$listid=$results.id
+
+#Read List ID by Name
+$apiUrl = "https://graph.microsoft.com/beta/sites/$($siteid)/lists"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Get
+$listName="Fruits"
+$listId=($results.value|select id,name|?{$_.name -eq $listName}).id
+
+#Add Apple,Kiwi,Avocado,Mango
+$fruits="Apple","Kiwi","Avocado","Mango"
+$location="Washington"
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists/$($listid)/items"
+foreach($fruit in $fruits){
+$data=@"
+{
+  "fields": {
+    "Title": "$($fruit)",
+    "Location": "$($location)"
+  }
+}
+"@
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Post -Body $data -ContentType "application/json"
+}
+
+#View Results
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists/$($listid)/items?expand=fields(select=Title,Location)"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Get
+$items=$results.value|select id,fields
+$listDetails=@() 
+foreach($item in $items)
+{
+$itemProperties = @{ID=$item.Id;Title=$item.fields.Title;Location=$item.fields.Location}
+$psThanguObject=New-Object PSObject –Property $itemProperties 
+$listDetails+=$psThanguObject 
+}$listDetails
+
+#View Results v1
+$apiUrl = "https://graph.microsoft.com/beta/sites/$($siteid)/lists/$($listid)"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Get
+$items=$results.value
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists/$($listid)/items"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Get
+$items=$results.value|select id,fields
+$listDetails=@() 
+foreach($item in $items)
+{
+$itemProperties = @{ID=$item.Id;Title=$item.fields.Title;Location=$item.fields.Location}
+$psThanguObject=New-Object PSObject –Property $itemProperties 
+$listDetails+=$psThanguObject 
+}$listDetails
+
+#Update App Location to Australia
+$appleID=($listDetails|?{$_.Title -eq "Apple"}|select ID).ID
+$appleID[0]
+$location="Australia"
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists/$($listid)/items/$($appleID[1])/fields"
+$data=@"
+{  "Title": "Apple",
+    "Location": "$($location)"}
+"@
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Patch -Body $data -ContentType "application/json"
+#Delete mango
+$mangoID=($listDetails|?{$_.Title -eq "Mango"}|select ID).ID
+$location="Australia"
+$apiUrl = "https://graph.microsoft.com/v1.0/sites/$($siteid)/lists/$($listid)/items/$($mangoID)"
+$results = Invoke-RestMethod -Headers @{Authorization = "Bearer $accessToken"} -Uri $apiUrl -Method Delete
+
